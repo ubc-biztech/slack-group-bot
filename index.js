413afcea1687d63008110ad27f45ebe5370f74ab
bennypc <52959626+bennypc@users.noreply.github.com>
2025-04-24 21:40:20 -0700

    socket checks

--- index.js
+++ index.js
@@ -206,3 +206,15 @@ const PORT = process.env.PORT || 3000
 server.listen(PORT, () => {
   console.log(`Dummy server listening on port ${PORT}`)
 })
+
+app.socketMode?.client?.on("disconnect", (e) => {
+  console.warn("Slack socket disconnected:", e)
+})
+
+app.socketMode?.client?.on("error", (e) => {
+  console.error("Slack socket error:", e)
+})
+
+app.socketMode?.client?.on("reconnect", () => {
+  console.log("Slack socket reconnecting...")
+})
